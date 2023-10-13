3c2b7aff268b4f1eb437e4461b12f062aaefc5af
israel-cj <93375286+israel-cj@users.noreply.github.com>
2023-10-13 12:43:33 +0200

    Reproduce

---
+++ frameworks/LLM_AutoML/setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+HERE=$(dirname "$0")
+VERSION=${1:-"latest"}
+REPO=${2:-"https://github.com/israel-cj/LLM_AutoML.git"}
+PKG=${3:-"LLM_AutoML"}
+if [[ "$VERSION" == "latest" ]]; then
+    VERSION="master"
+fi
+
+#create local venv
+. ${HERE}/../shared/setup.sh ${HERE} true
+
+PIP install -r ${HERE}/requirements.txt
+
+#    PIP install --no-cache-dir -e git+${REPO}@${VERSION}#egg=${PKG}
+TARGET_DIR="${HERE}/lib/${PKG}"
+rm -Rf ${TARGET_DIR}
+git clone ${REPO} ${TARGET_DIR}
+PIP install -U -e ${TARGET_DIR}
+
+
+
