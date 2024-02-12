1eba9026391cbfcf8e60441d370caa79d921380c
israel-cj <93375286+israel-cj@users.noreply.github.com>
2024-02-12 14:26:08 +0100

    PP_LLM added

---
+++ frameworks/PP_LLM/setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+HERE=$(dirname "$0")
+VERSION=${1:-"latest"}
+REPO=${2:-"https://github.com/israel-cj/pp_llm.git"}
+PKG=${3:-"PP_LLM"}
+if [[ "$VERSION" == "latest" ]]; then
+    VERSION="master"
+fi
+
+#create local venv
+. ${HERE}/../shared/setup.sh ${HERE} true
+
+PIP install -r ${HERE}/requirements.txt
+
+#    PIP install --no-cache-dir -e git+${REPO}@${VERSION}#egg=${PKG}
+TARGET_DIR="${HERE}/lib/${PKG}"
+rm -Rf ${TARGET_DIR}
+git clone ${REPO} ${TARGET_DIR}
+PIP install -U -e ${TARGET_DIR}
+
+
+
